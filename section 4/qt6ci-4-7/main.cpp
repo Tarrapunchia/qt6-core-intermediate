9b1750a9513de99565b896803d87057d0fb81d99
tarrapunchia <f.zuc87@gmail.com>
2025-06-22 14:10:56 +0200

    notes on section 4-7: QScopedPointer

--- section 4/qt6ci-4-7/main.cpp
+++ section 4/qt6ci-4-7/main.cpp
@@ -8,6 +8,15 @@
 
   How
   See below
+  Sono puntatori scoped, semplicemente
+  Incluso in :
+    #include <QScopedPointer>
+  dichiarazione :
+    template <typename T, typename Cleanup = QScopedPointerDeleter<T>>
+    class QScopedPointer {...}
+
+  a questo pounto l'arrow op accede direttamente ai metodi dell'oggetto puntato
+  non dello smart ptr come in c++ (non servono get o set via)
 */
 
 #include <QCoreApplication>
